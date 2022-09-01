371f6971e40737cd667e42a6a3a567a5f0e99f48
Gianni <gstclair19@gmail.com>
2022-09-01 08:15:16 -0700

    Adding PostgreSQL

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ from flask_gravatar import Gravatar
 from functools import wraps
 from flask import abort
 from flask_gravatar import Gravatar
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
